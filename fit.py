7654a93bbdb920dc0851358322d45c344c1cdbc8
Adam Sun <59337565+Asun13@users.noreply.github.com>
2022-12-05 17:02:13 -0800

    Add files via upload
    Uploaded code

---
+++ fit.py
@@ -0,0 +1,537 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 15 19:57:47 2022
+
+@author: adamsun
+"""
+
+#@title
+import concurrent.futures
+import collections
+import dataclasses
+import hashlib
+import itertools
+import json
+import math
+import os
+import pathlib
+import random
+import re
+import string
+import time
+import urllib.request
+
+import einops
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from PIL import Image
+import requests
+import tqdm
+from sklearn.model_selection import train_test_split
+
+import tensorflow as tf
+def flickr8k(path='flickr8k'):
+  path = pathlib.Path(path)
+
+  if len(list(path.rglob('*'))) < 16197:
+    tf.keras.utils.get_file(
+        origin='https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_Dataset.zip',
+        cache_dir='.',
+        cache_subdir=path,
+        extract=True)
+    tf.keras.utils.get_file(
+        origin='https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_text.zip',
+        cache_dir='.',
+        cache_subdir=path,
+        extract=True)
+
+  captions = (path/"Flickr8k.token.txt").read_text().splitlines()
+  captions = (line.split('\t') for line in captions)
+  captions = ((fname.split('#')[0], caption) for (fname, caption) in captions)
+
+  cap_dict = collections.defaultdict(list)
+  for fname, cap in captions:
+    cap_dict[fname].append(cap)
+
+  train_files = (path/'Flickr_8k.trainImages.txt').read_text().splitlines()
+  train_captions = [(str(path/'Flicker8k_Dataset'/fname), cap_dict[fname]) for fname in train_files]
+  print(train_captions)
+  test_files = (path/'Flickr_8k.testImages.txt').read_text().splitlines()
+  test_captions = [(str(path/'Flicker8k_Dataset'/fname), cap_dict[fname]) for fname in test_files]
+
+  train_ds = tf.data.experimental.from_list(train_captions)
+  test_ds = tf.data.experimental.from_list(test_captions)
+
+  return train_ds, test_ds
+import sklearn
+def preprocess(text):
+    replacedict = {"-": " ", "how to ": "", "make": "", "easy": ""}
+    text = text.lower()
+    for key in replacedict.keys():
+        text = text.replace(key, replacedict[key])
+    return text
+def food(path='epicurious'):
+  data = pd.read_csv("combined_data/combined.csv")
+  captions = data["caption"].tolist()
+  image_paths = data["path"].tolist()
+  for i in range(len(image_paths)):
+      path_processed = image_paths[i].split(".jpg")[0]
+      image_paths[i] = f"combined_data/images/{path_processed}.jpg"
+  image_train, image_test, caption_train, caption_test = sklearn.model_selection.train_test_split(image_paths, captions, test_size = 0.2, random_state = 0)
+  train_images = []
+  train_captions = []
+  test_images = []
+  test_captions = []
+  for i in range(len(caption_train)):
+    if not pd.isna(caption_train[i]) and not pd.isna(image_train[i]):
+        train_images.append(image_train[i])
+        train_captions.append([preprocess(caption_train[i])])
+  for i in range(len(caption_test)):
+    if not pd.isna(caption_test[i]) and not pd.isna(image_test[i]):
+        test_images.append(image_test[i])
+        test_captions.append([preprocess(caption_test[i])])
+  train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_captions))
+  test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_captions))
+  return train_ds, test_ds
+train_raw, test_raw = food()
+print(len(train_raw))
+print(len(test_raw))
+
+for ex_path, ex_captions in train_raw.take(1):
+  print(ex_path)
+  print(ex_captions)
+
+IMAGE_SHAPE=(224, 224, 3)
+
+mobilenet = tf.keras.applications.efficientnet.EfficientNetB0(
+    input_shape=IMAGE_SHAPE,
+    include_top=False)
+mobilenet.trainable=False
+
+def load_image(image_path):
+    img = tf.io.read_file(image_path)
+    img = tf.io.decode_jpeg(img, channels=3)
+    img = tf.image.resize(img, IMAGE_SHAPE[:-1])
+    return img
+test_img_batch = load_image(ex_path)[tf.newaxis, :]
+
+print(test_img_batch.shape)
+print(mobilenet(test_img_batch).shape)
+
+def standardize(s):
+  s = tf.strings.lower(s)
+  s = tf.strings.regex_replace(s, f'[{re.escape(string.punctuation)}]', '')
+  s = tf.strings.join(['[START]', s, '[END]'], separator=' ')
+  return s
+# Use the top 5000 words for a vocabulary.
+vocabulary_size = 5000
+tokenizer = tf.keras.layers.TextVectorization(
+    max_tokens=vocabulary_size,
+    standardize=standardize,
+    ragged=True)
+tokenizer.adapt(train_raw.map(lambda fp,txt: txt).unbatch().batch(1024))
+
+
+
+# Create mappings for words to indices and indices to words.
+word_to_index = tf.keras.layers.StringLookup(
+    mask_token="",
+    vocabulary=tokenizer.get_vocabulary())
+index_to_word = tf.keras.layers.StringLookup(
+    mask_token="",
+    vocabulary=tokenizer.get_vocabulary(),
+    invert=True)
+
+def match_shapes(images, captions):
+  caption_shape = einops.parse_shape(captions, 'b c')
+  captions = einops.rearrange(captions, 'b c -> (b c)')
+  images = einops.repeat(
+      images, 'b ... -> (b c) ...',
+      c = caption_shape['c'])
+  return images, captions
+
+for ex_paths, ex_captions in train_raw.batch(32).take(1):
+  break
+
+print('image paths:', ex_paths.shape)
+print('captions:', ex_captions.shape)
+print()
+
+ex_paths, ex_captions = match_shapes(images=ex_paths, captions=ex_captions)
+
+print('image_paths:', ex_paths.shape)
+print('captions:', ex_captions.shape)
+
+def prepare_txt(imgs, txts):
+  tokens = tokenizer(txts)
+
+  input_tokens = tokens[..., :-1]
+  label_tokens = tokens[..., 1:]
+  return (imgs, input_tokens), label_tokens
+def prepare_dataset(ds, tokenizer, batch_size=64, shuffle_buffer=1000):
+  # Load the images and make batches.
+  ds = (ds
+        .shuffle(10000)
+        .map(lambda path, caption: (load_image(path), caption))
+        .apply(tf.data.experimental.ignore_errors())
+        .batch(batch_size))
+
+  def to_tensor(inputs, labels):
+    (images, in_tok), out_tok = inputs, labels
+    return (images, in_tok.to_tensor()), out_tok.to_tensor()
+
+  return (ds
+          .map(match_shapes, tf.data.AUTOTUNE)
+          .unbatch()
+          .shuffle(shuffle_buffer)
+          .batch(batch_size)
+          .map(prepare_txt, tf.data.AUTOTUNE)
+          .map(to_tensor, tf.data.AUTOTUNE)
+          )
+
+def save_dataset(ds, save_path, image_model, tokenizer, shards=10, batch_size=32):
+  # Load the images and make batches.
+  ds = (ds
+        .map(lambda path, caption: (load_image(path), caption))
+        .apply(tf.data.experimental.ignore_errors())
+        .batch(batch_size))
+
+  # Run the feature extractor on each batch
+  # Don't do this in a .map, because tf.data runs on the CPU. 
+  def gen():
+    for (images, captions) in tqdm.tqdm(ds): 
+      feature_maps = image_model(images)
+
+      feature_maps, captions = match_shapes(feature_maps, captions)
+      yield feature_maps, captions
+
+  # Wrap the generator in a new tf.data.Dataset.
+  new_ds = tf.data.Dataset.from_generator(
+      gen,
+      output_signature=(
+          tf.TensorSpec(shape=image_model.output_shape),
+          tf.TensorSpec(shape=(None,), dtype=tf.string)))
+
+  # Apply the tokenization 
+  new_ds = (new_ds
+            .map(prepare_txt, tf.data.AUTOTUNE)
+            .unbatch()
+            .shuffle(1000))
+
+  # Save the dataset into shard files.
+  def shard_func(i, item):
+    return i % shards
+  new_ds.enumerate().save(save_path, shard_func=shard_func)
+
+def load_dataset(save_path, batch_size=32, shuffle=1000, cycle_length=2):
+  def custom_reader_func(datasets):
+    datasets = datasets.shuffle(1000)
+    return datasets.interleave(lambda x: x, cycle_length=cycle_length)
+
+  ds = tf.data.Dataset.load(save_path, reader_func=custom_reader_func)
+
+  def drop_index(i, x):
+    return x
+
+  ds = (ds
+        .map(drop_index, tf.data.AUTOTUNE)
+        .shuffle(shuffle)
+        .padded_batch(batch_size)
+        .prefetch(tf.data.AUTOTUNE))
+  return ds
+
+
+save_dataset(train_raw, 'train_cache', mobilenet, tokenizer)
+save_dataset(test_raw, 'test_cache', mobilenet, tokenizer)
+train_ds = load_dataset('train_cache')
+test_ds = load_dataset('test_cache')
+train_ds.element_spec
+
+class SeqEmbedding(tf.keras.layers.Layer):
+  def __init__(self, vocab_size, max_length, depth):
+    super().__init__()
+    self.pos_embedding = tf.keras.layers.Embedding(input_dim=max_length, output_dim=depth)
+
+    self.token_embedding = tf.keras.layers.Embedding(
+        input_dim=vocab_size,
+        output_dim=depth,
+        mask_zero=True)
+
+    self.add = tf.keras.layers.Add()
+
+  def call(self, seq):
+    seq = self.token_embedding(seq) # (batch, seq, depth)
+
+    x = tf.range(tf.shape(seq)[1])  # (seq)
+    x = x[tf.newaxis, :]  # (1, seq)
+    x = self.pos_embedding(x)  # (1, seq, depth)
+
+    return self.add([seq,x])
+class CausalSelfAttention(tf.keras.layers.Layer):
+  def __init__(self, **kwargs):
+    super().__init__()
+    self.mha = tf.keras.layers.MultiHeadAttention(**kwargs)
+    # Use Add instead of + so the keras mask propagates through.
+    self.add = tf.keras.layers.Add() 
+    self.layernorm = tf.keras.layers.LayerNormalization()
+
+  def call(self, x):
+    attn = self.mha(query=x, value=x,
+                    use_causal_mask=True)
+    x = self.add([x, attn])
+    return self.layernorm(x)
+class CrossAttention(tf.keras.layers.Layer):
+  def __init__(self,**kwargs):
+    super().__init__()
+    self.mha = tf.keras.layers.MultiHeadAttention(**kwargs)
+    self.add = tf.keras.layers.Add() 
+    self.layernorm = tf.keras.layers.LayerNormalization()
+
+  def call(self, x, y, **kwargs):
+    attn, attention_scores = self.mha(
+             query=x, value=y,
+             return_attention_scores=True)
+
+    self.last_attention_scores = attention_scores
+
+    x = self.add([x, attn])
+    return self.layernorm(x)
+class FeedForward(tf.keras.layers.Layer):
+  def __init__(self, units, dropout_rate=0.1):
+    super().__init__()
+    self.seq = tf.keras.Sequential([
+        tf.keras.layers.Dense(units=2*units, activation='relu'),
+        tf.keras.layers.Dense(units=units),
+        tf.keras.layers.Dropout(rate=dropout_rate),
+    ])
+
+    self.layernorm = tf.keras.layers.LayerNormalization()
+
+  def call(self, x):
+    x = x + self.seq(x)
+    return self.layernorm(x)
+class DecoderLayer(tf.keras.layers.Layer):
+  def __init__(self, units, num_heads=1, dropout_rate=0.1):
+    super().__init__()
+
+    self.self_attention = CausalSelfAttention(num_heads=num_heads,
+                                              key_dim=units,
+                                              dropout=dropout_rate)
+    self.cross_attention = CrossAttention(num_heads=num_heads,
+                                          key_dim=units,
+                                          dropout=dropout_rate)
+    self.ff = FeedForward(units=units, dropout_rate=dropout_rate)
+
+
+  def call(self, inputs, training=False):
+    in_seq, out_seq = inputs
+
+    # Text input
+    out_seq = self.self_attention(out_seq)
+
+    out_seq = self.cross_attention(out_seq, in_seq)
+
+    self.last_attention_scores = self.cross_attention.last_attention_scores
+
+    out_seq = self.ff(out_seq)
+
+    return out_seq
+class TokenOutput(tf.keras.layers.Layer):
+  def __init__(self, tokenizer, banned_tokens=('', '[UNK]', '[START]'), **kwargs):
+    super().__init__()
+
+    self.dense = tf.keras.layers.Dense(
+        units=tokenizer.vocabulary_size(), **kwargs)
+    self.tokenizer = tokenizer
+    self.banned_tokens = banned_tokens
+
+    self.bias = None
+
+  def adapt(self, ds):
+    counts = collections.Counter()
+    vocab_dict = {name: id 
+                  for id, name in enumerate(self.tokenizer.get_vocabulary())}
+
+    for tokens in tqdm.tqdm(ds):
+      counts.update(tokens.numpy().flatten())
+
+    counts_arr = np.zeros(shape=(self.tokenizer.vocabulary_size(),))
+    counts_arr[np.array(list(counts.keys()), dtype=np.int32)] = list(counts.values())
+
+    counts_arr = counts_arr[:]
+    for token in self.banned_tokens:
+      counts_arr[vocab_dict[token]] = 0
+
+    total = counts_arr.sum()
+    p = counts_arr/total
+    p[counts_arr==0] = 1.0
+    log_p = np.log(p)  # log(1) == 0
+
+    entropy = -(log_p*p).sum()
+
+    print()
+    print(f"Uniform entropy: {np.log(self.tokenizer.vocabulary_size()):0.2f}")
+    print(f"Marginal entropy: {entropy:0.2f}")
+
+    self.bias = log_p
+    self.bias[counts_arr==0] = -1e9
+
+  def call(self, x):
+    x = self.dense(x)
+    # TODO(b/250038731): Fix this.
+    # An Add layer doesn't work because of the different shapes.
+    # This clears the mask, that's okay because it prevents keras from rescaling
+    # the losses.
+    return x + self.bias
+    
+
+output_layer = TokenOutput(tokenizer, banned_tokens=('', '[UNK]', '[START]'))
+# This might run a little faster if the dataset didn't also have to load the image data.
+output_layer.adapt(train_ds.map(lambda inputs, labels: labels))
+
+class Captioner(tf.keras.Model):
+  @classmethod
+  def add_method(cls, fun):
+    setattr(cls, fun.__name__, fun)
+    return fun
+
+  def __init__(self, tokenizer, feature_extractor, output_layer, num_layers=1,
+               units=256, max_length=50, num_heads=1, dropout_rate=0.1):
+    super().__init__()
+    self.feature_extractor = feature_extractor
+    self.tokenizer = tokenizer
+    self.word_to_index = tf.keras.layers.StringLookup(
+        mask_token="",
+        vocabulary=tokenizer.get_vocabulary())
+    self.index_to_word = tf.keras.layers.StringLookup(
+        mask_token="",
+        vocabulary=tokenizer.get_vocabulary(),
+        invert=True) 
+
+    self.seq_embedding = SeqEmbedding(
+        vocab_size=tokenizer.vocabulary_size(),
+        depth=units,
+        max_length=max_length)
+
+    self.decoder_layers = [
+        DecoderLayer(units, num_heads=num_heads, dropout_rate=dropout_rate)
+        for n in range(num_layers)]
+
+    self.output_layer = output_layer
+  def call(self, inputs):
+    image, txt = inputs
+
+    if image.shape[-1] == 3:
+      # Apply the feature-extractor, if you get an RGB image.
+      image = self.feature_extractor(image)
+
+    # Flatten the feature map
+    image = einops.rearrange(image, 'b h w c -> b (h w) c')
+
+
+    if txt.dtype == tf.string:
+      # Apply the tokenizer if you get string inputs.
+      txt = tokenizer(txt)
+
+    txt = self.seq_embedding(txt)
+
+    # Look at the image
+    for dec_layer in self.decoder_layers:
+      txt = dec_layer(inputs=(image, txt))
+
+    txt = self.output_layer(txt)
+
+    return txt
+
+
+model = Captioner(tokenizer, feature_extractor=mobilenet, output_layer=output_layer,
+                  units=256, dropout_rate=0.5, num_layers=2, num_heads=2)
+
+
+@Captioner.add_method
+def simple_gen(self, image, temperature=1):
+  initial = self.word_to_index([['[START]']]) # (batch, sequence)
+  img_features = self.feature_extractor(image[tf.newaxis, ...])
+
+  tokens = initial # (batch, sequence)
+  for n in range(50):
+    preds = self((img_features, tokens)).numpy()  # (batch, sequence, vocab)
+    preds = preds[:,-1, :]  #(batch, vocab)
+    if temperature==0:
+        next = tf.argmax(preds, axis=-1)[:, tf.newaxis]  # (batch, 1)
+    else:
+        next = tf.random.categorical(preds/temperature, num_samples=1)  # (batch, 1)
+    tokens = tf.concat([tokens, next], axis=1) # (batch, sequence) 
+
+    if next[0] == self.word_to_index('[END]'):
+      break
+  words = index_to_word(tokens[0, 1:-1])
+  result = tf.strings.reduce_join(words, axis=-1, separator=' ')
+  return result.numpy().decode()
+
+def masked_loss(labels, preds):  
+  loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels, preds)
+
+  mask = (labels != 0) & (loss < 1e8) 
+  mask = tf.cast(mask, loss.dtype)
+
+  loss = loss*mask
+  loss = tf.reduce_sum(loss)/tf.reduce_sum(mask)
+  return loss
+
+def masked_acc(labels, preds):
+  mask = tf.cast(labels!=0, tf.float32)
+  preds = tf.argmax(preds, axis=-1)
+  labels = tf.cast(labels, tf.int64)
+  match = tf.cast(preds == labels, mask.dtype)
+  acc = tf.reduce_sum(match*mask)/tf.reduce_sum(mask)
+  return acc
+
+class GenerateText(tf.keras.callbacks.Callback):
+  def __init__(self):
+    self.image = load_image("ribeye.jpg")
+
+  def on_epoch_end(self, epochs=None, logs=None):
+    print()
+    print()
+    for t in (0.0, 0.5, 1.0):
+      result = self.model.simple_gen(self.image, temperature=t)
+      print(result)
+    print()
+callbacks = [
+    GenerateText()]
+    #tf.keras.callbacks.EarlyStopping(
+        #patience=5, restore_best_weights=True)]
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
+           loss=masked_loss,
+           metrics=[masked_acc])
+train_ds.concatenate(test_ds)
+history = model.fit(
+    train_ds.repeat(),
+    steps_per_epoch=128,
+    #validation_data=test_ds.repeat(),
+   # validation_steps=20,
+    epochs=20,
+    callbacks=callbacks)
+model.save_weights('my_weights')
+with open('results_0.txt', 'w') as f:
+    for image_path in os.listdir("test"):
+        ground_truth = image_path.split(".")[0]
+        image = load_image(f"test/{image_path}")
+        result = model.simple_gen(image, temperature=0)
+        f.write(f"{ground_truth}, {result}\n")
+with open('results_0.5.txt', 'w') as f:
+    for image_path in os.listdir("test"):
+        ground_truth = image_path.split(".")[0]
+        image = load_image(f"test/{image_path}")
+        result = model.simple_gen(image, temperature=0.5)
+        f.write(f"{ground_truth}, {result}\n")
+with open('results_1.txt', 'w') as f:
+    for image_path in os.listdir("test"):
+        ground_truth = image_path.split(".")[0]
+        image = load_image(f"test/{image_path}")
+        result = model.simple_gen(image, temperature=1)
+        f.write(f"{ground_truth}, {result}\n")
+    
